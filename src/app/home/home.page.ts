01a0a6d4ca66e2a8f5f0e1e889c53da41ac52431
Saulson <saulson.96@gmail.com>
2021-06-05 23:21:20 -0500

    Se crea app para hacer un timer

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Insomnia } from '@ionic-native/insomnia/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,92 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  percent:number = 50;
+  radius:number = 100;
+  progress:any = 0;
+  minutes:number = 1;
+  seconds:any = 30;
+  fullTime:any = '00:01:30';
+  timer:any = false;
+  overallTimer:any = false;
+  elapsed:any = {
+    h: '00',
+    m: '00',
+    s: '00',
+  }
+
+  constructor(private insomnia:Insomnia) {}
+
+  startTimer() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+    if (!this.overallTimer) {
+      this.progressTimer();
+      this.insomnia.keepAwake();
+    }
+
+    this.timer = false;
+    this.percent = 0;
+    this.progress = 0;
+
+    let timeSplit = this.fullTime.split(':');
+    this.minutes = timeSplit[1];
+    this.seconds = timeSplit[2];
+
+    let totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
+
+    this.timer = setInterval(() => {
+      if (this.percent == this.radius) {
+        clearInterval(this.timer);
+      }
+
+      this.percent = Math.floor((this.progress / totalSeconds) * 100);
+      this.progress++;
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+    clearInterval(this.overallTimer);
+
+    this.overallTimer = false;
+    this.timer = false;
+    this.percent = 0;
+    this.progress = 0;
+    this.elapsed = {
+      h: '00',
+      m: '00',
+      s: '00',
+    }
+    this.insomnia.allowSleepAgain();
+  }
+
+  progressTimer() {
+    let countDownDate = new Date().getTime();
+
+    this.overallTimer = setInterval(() => {
+      let now = new Date().getTime();
+      let distance = now - countDownDate;
+
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
+
+      this.elapsed.h = this.pad(this.elapsed.h, 2);
+      this.elapsed.m = this.pad(this.elapsed.m, 2);
+      this.elapsed.s = this.pad(this.elapsed.s, 2);
+    });
+  }
+
+  pad(num, size) {
+    let res = num + "";
+
+    while (res.length < size) {
+      res = "0" + res;
+    }
+
+    return res;
+  }
 
 }
